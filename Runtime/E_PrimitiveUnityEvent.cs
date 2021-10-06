1d42d7d2f4842ac7e4d1a368c8669d2b81348072
Eloi 4 ActiveMe <eloi4activeme@gmail.com>
2021-10-06 16:00:15 +0200

    Save: 10/06/2021 4:00 PM

---
+++ Runtime/E_PrimitiveUnityEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Eloi
+{
+    [System.Serializable]
+    public class PrimitiveUnityEvent_Short : UnityEvent<short> { }
+    [System.Serializable]
+    public class PrimitiveUnityEvent_Byte : UnityEvent<byte> { }
+    [System.Serializable]
+    public class PrimitiveUnityEvent_Int : UnityEvent<int> { }
+
+    [System.Serializable]
+    public class PrimitiveUnityEvent_Float : UnityEvent<float> { }
+    [System.Serializable]
+    public class PrimitiveUnityEvent_Long : UnityEvent<long> { }
+    [System.Serializable]
+    public class PrimitiveUnityEvent_Doube : UnityEvent<double> { }
+
+    [System.Serializable]
+    public class PrimitiveUnityEvent_Bool : UnityEvent<bool> { }
+    [System.Serializable]
+    public class PrimitiveUnityEvent_String : UnityEvent<string> { }
+    [System.Serializable]
+    public class PrimitiveUnityEvent_DoubleStirng : UnityEvent<string, string> { }
+
+
+}
