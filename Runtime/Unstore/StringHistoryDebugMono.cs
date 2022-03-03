b7a984423364f6ee0b63c1d1f7ea2aa9caed6a93
Eloi Strée <eloi.stree@activeme.be>
2022-03-04 00:06:15 +0100

    Add old cool script and some inputfield ui that should be in project

---
+++ Runtime/Unstore/StringHistoryDebugMono.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StringHistoryDebugMono : MonoBehaviour
+{
+    public Eloi.StringClampHistory m_debugHistory;
+
+    public void PushIn(string id) {
+        m_debugHistory.PushIn(id);
+    }
+    //public void Clear() {
+    //    m_debugHistory.Clear();
+    //}
+}
