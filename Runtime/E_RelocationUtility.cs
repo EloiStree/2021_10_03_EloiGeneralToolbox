6f8e16efeacfd0b0d7fe8df3ac9a492dee8fcfcd
Eloi Strée <eloi.stree@activeme.be>
2022-05-05 02:12:36 +0200

    Add rotate and drawing plus a tranform player perf

--- Runtime/E_RelocationUtility.cs
+++ Runtime/E_RelocationUtility.cs
@@ -46,5 +46,15 @@ namespace Eloi {
             worldRotation = localRotation * rotationReference;
             worldPosition = (rotationReference * localPosition) + (positionReference);
         }
+
+        public static Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles)
+        {
+            return RotatePointAroundPivot(point, pivot, Quaternion.Euler(angles));
+        }
+
+        public static Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion rotation)
+        {
+            return rotation * (point - pivot) + pivot;
+        }
     }
 }
