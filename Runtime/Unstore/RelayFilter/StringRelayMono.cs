9a19e30718f75deb2e9715ea8712024d88c7111b
Eloi Strée <eloi.stree@activeme.be>
2022-05-16 00:21:26 +0200

    Save from locker experiment project

--- Runtime/Unstore/RelayFilter/StringRelayMono.cs
+++ Runtime/Unstore/RelayFilter/StringRelayMono.cs
@@ -6,6 +6,18 @@ public class StringRelayMono : MonoBehaviour
 {
     public bool dontPushEmpty=true;
     public Eloi.PrimitiveUnityEvent_String m_textToRelay;
+
+    [ContextMenu("Push With Empty String")]
+    public void PushWithEmptyString()
+    {
+        PushInTextToRelay("");
+    }
+    [ContextMenu("Push With Null")]
+    public void PushWithNull()
+    {
+        PushInTextToRelay(null);
+    }
+
     public void PushInTextToRelay(string text) {
         if (dontPushEmpty && Eloi.E_StringUtility.IsNullOrEmpty(in text))
             return;
